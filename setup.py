6bd3f71401fd355b7cb865e1030c8127b64a0e08
aureliusnoble <aureliusnoble@gmail.com>
2023-03-08 10:08:01 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='neuralnet-fs',
+    version=__version__,
+
+    url='https://github.com/aureliusnoble/neuralnet-fs/new/main',
+    author='Aurelius Noble',
+    author_email='a.j.noble@lse.com',
+
+    py_modules=['nnfs'],
+)
