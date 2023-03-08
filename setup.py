c4b85f8b9c1e731a108cd4c2284e7f82fcdcb4e3
aureliusnoble <aureliusnoble@gmail.com>
2023-03-08 10:11:25 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='neuralnet-fs',
-    version=0.0.1,
+    version='0.0.1',
 
     url='https://github.com/aureliusnoble/neuralnet-fs/new/main',
     author='Aurelius Noble',
